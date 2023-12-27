b9b8930f7dc13d1ed2941e3461742667db6dd1cf
adriano22_ <califano993@gmail.com>
2023-12-27 18:02:46 +0100

    web scraping code
    Added file for web scraping functions

--- src/graph.py
+++ src/graph.py
@@ -12,7 +12,7 @@ class Graph():
         self.add_node(paper)
         self.add_node(cited_paper)
         
-        self.__graph[paper.eid].add(cited_paper)
+        self.__graph[paper.eid].add(cited_paper.eid)
         
     def remove_edge(self, paper: Paper, cited_paper: Paper):
         self.__graph[paper.eid].remove(cited_paper.eid)
@@ -24,16 +24,26 @@ class Graph():
     
     def get_connections(self, paper):
         return self.__graph[paper.eid]
+    
+    def get_edges(self):
+        edges = []
+        
+        for node in self.__graph:
+            for con in self.__graph[node]:
+                edges.append( (node, con) )
+        
+        return edges
         
         
 if __name__ == "__main__":
-    paper1 = Paper("Paper poco scientifico", ["Gianmarco Tocco", "Dario Moccia"], ["twitch", "content", "stream"], 
-                   "s2023-twitch123", "Article", "Twitch.tv", 2023)
+    paper1 = Paper("Paper poco scientifico", "s2023-twitch123", "Article", 2023)
     
-    paper2 = Paper("Paper molto scientifico", ["Bruno Vespa", "Mario Giordano"], ["Repubblica", "Italia"],
-                   "s2023-rep1", "Conference Paper", "Rai", 2022)
+    paper2 = Paper("Paper molto scientifico", "s2023-rep1", "Conference Paper", 2022)
     
     graph = Graph()
     graph.add_edge(paper1, paper2)
     print(graph.get_connections(paper1))
     print(graph.get_connections(paper2))
+    
+    
+    print(graph.get_edges())
--- src/paper.py
+++ src/paper.py
@@ -1,25 +1,14 @@
 class Paper():
-    def __init__(self, title: str, authors: list, keywords: list, eid: str, document_type: str, publisher: str, year: int) -> None:
+    def __init__(self, title: str, eid: str, document_type: str, year: int) -> None:
         self.__title = title
-        self.__authors = authors
-        self.__keywords = keywords
         self.__eid = eid
         self.__document_type = document_type,
-        self.__publisher = publisher
         self.__year = year
 
     @property
     def title(self):
         return self.__title
-
-    @property
-    def authors(self):
-        return self.__authors
-
-    @property
-    def keywords(self):
-        return self.__keywords
-
+    
     @property
     def eid(self):
         return self.__eid
@@ -27,17 +16,12 @@ class Paper():
     @property
     def document_type(self):
         return self.__document_type
-    
-    @property
-    def publisher(self):
-        return self.__publisher
 
     @property
     def year(self):
         return self.__year
     
 if __name__ == "__main__":
-    paper = Paper("Paper poco scientifico", ["Gianmarco Tocco", "Dario Moccia"], ["twitch", "content", "stream"], 
-                  "s2023-twitch123", "Article", "Twitch.tv", 2023)
+    paper = Paper("Paper poco scientifico", "s2023-twitch123", "Article", 2023)
     
-    print(paper.title, paper.authors, paper.keywords, paper.eid, paper.document_type, paper.publisher, paper.year)
+    print(paper.title, paper.eid, paper.document_type, paper.year)
